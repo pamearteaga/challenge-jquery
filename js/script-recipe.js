9253b094239ce007ea9ad3f24568468111c12b6d
pamearteaga <pe_arteaga@hotmail.com>
2018-01-08 21:23:39 -0300

    Etapa 100: funcion flecha back

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,10 +1,15 @@
 $(document).ready( function(){
 
+  //ocultar boton menu (ETAPA 1)
+  $(".js-menu").hide();
+
   //ETAPA 7
   console.log("Listo :-)");
 
-  //ocultar boton menu (ETAPA 1)
-  $(".js-menu").hide();
+  //ETAPA 10
+  $(".js-back").click(function() {
+    $("#back").attr("href", "index.html")
+  });
 
 });
 
@@ -20,3 +25,5 @@ $(".js-show-recipe").click(function() {
   $(".js-show-make").removeClass("active");
   $(".js-show-recipe").addClass("active");
 });
+
+
